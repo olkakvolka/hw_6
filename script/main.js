86a44317e306b4b15d7a4a33dde9a9f28d0c316e
Olgha Kvyatkevich <olkakvolka@gmail.com>
2017-12-28 10:27:20 +0300

    modified_print_todolist

--- script/main.js
+++ script/main.js
@@ -1,38 +1,52 @@
 window.onload = function() {
-var todolist = [];
+	var todolist = [];
+	var list = document.querySelector('ul');
 
-add_item.addEventListener("click", function() {
-	var item = {};
-	if (item.todo = document.getElementById("input_item").value === "") {
-		alert("What need to do?")
-	} else {
-		item.todo = document.getElementById("input_item").value;
-		item.check = false;
-	 	todolist.push(item);
-	 	out(item);
-	 	add_item = document.getElementById("input_item").value = ""
+
+	add_item.addEventListener("click", function() {
+		var item = {};
+		var new_item = document.getElementById("input_item").value;
+		if (new_item === "") {
+			alert("What need to do?");
+		} else {
+			item.todo = new_item;
+			item.check = false;
+			todolist.push(item);
+			print_todo_item();
+			document.getElementById("input_item").value = "";
+			}
+	})
+
+	function print_todo_item() {
+		while (list.lastChild) {
+			list.removeChild(list.lastChild);
+		}
+		todolist.forEach(function(item) {
+			var out_li = document.createElement("li");	
+			out_li.innerHTML = item.todo;
+			out_ul.appendChild(out_li);
+			
+			if (item.check) {
+			out_li.classList.add('checked');
+			}
+		});
 	}
-})
-
-function out(item) {
-	var out_li = document.createElement("li");
-	out_li.innerHTML = item.todo;
-	out_ul.appendChild(out_li);
-}
-
-var list = document.querySelector('ul')
-list.addEventListener("click", function() {
-	event.target.style.textDecoration = 'line-through';
 
 
-}, false)
+	list.addEventListener("click", function() {
+		event.target.classList.add('checked');
+	})
 
-clear_item.addEventListener("click", function() {
-	todolist.length = 0;
-	while (list.lastChild) {
-    list.removeChild(list.lastChild);
-  }
-})
+	clear_item.addEventListener("click", function() {
+		
+		while (list.lastChild) {
+			list.removeChild(list.lastChild);
+		}
+	})
+	
+	clear_item.addEventListener("click", function() {
+		todolist.length = 0;
+	})
 
 }
 
